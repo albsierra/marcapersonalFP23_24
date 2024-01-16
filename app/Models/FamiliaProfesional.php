d8564cd0fdbe152497035ea83cfc0a5b58276a09
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-01-16 13:21:40 +0100

    Controlador de recursos de familias profesionales

---
+++ app/Models/FamiliaProfesional.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FamiliaProfesional extends Model
+{
+    use HasFactory;
+
+    protected $table = 'familias_profesionales';
+
+    protected $fillable = [
+        'codigo',
+        'nombre',
+    ];
+
+}
